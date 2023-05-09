fa340127a24970ccd367128ff4eed2f574bfd6fa
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-09 11:28:18 +0200

    add graphics charts

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}')
+const lat = 49.3580;
+const lon = 6.1655;
+const apiKey = '812f27a183a14d0a2752278634e7def5'; 
+
+// Construct the API URL
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+// Make the API call
+fetch(apiUrl)
   .then(response => response.json())
-  .then(datas => {
-    generateProjects(datas);
+  .then(data => {
+    // Process the weather data
+    console.log(data);
+    // You can access specific weather information from the 'data' object
   })
   .catch(error => {
-    console.error('Error fetching projects:', error);
+    console.log('Error:', error);
   });
-})
+
